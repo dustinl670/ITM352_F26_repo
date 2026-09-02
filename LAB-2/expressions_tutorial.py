7a1460f6fec5231a86cc4a08be9df98cdce69414
dustlope670 <dustinal@hawaii.edu>
2026-09-01 23:18:48 -1000

    Added expressions_tutorial.py file to LAB-2 dir.

---
+++ LAB-2/expressions_tutorial.py
@@ -0,0 +1,6 @@
+name = "Dustin"
+age = 21
+major = "Management Information Systems"
+grad_year = 2027
+
+print(f"My name is {name}, I am {age} years old, and I am majoring in {major}. I plan to graduate in {grad_year}.")
